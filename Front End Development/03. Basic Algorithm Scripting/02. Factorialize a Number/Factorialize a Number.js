710514b85a2bbff323b0807bbd8a272f62f50329
elminsterrr <elminsterthewhite@gmail.com>
2016-05-04 16:41:46 +0200

    Add comments

--- Front End Development/03. Basic Algorithm Scripting/02. Factorialize a Number/Factorialize a Number.js
+++ Front End Development/03. Basic Algorithm Scripting/02. Factorialize a Number/Factorialize a Number.js
@@ -1,8 +1,8 @@
-function factorialize(num) {
-  if ( num === 0 ) {
+function factorialize(num) {              //Funkcja, która wylicza silnię z podanego argumentu.
+  if ( num === 0 ) {                      //Jeśli argument wprowadzony w miejsce parametru "num" będzie równy "0" to zwróć 1. Tym samym kończy działanie funkcji i pętlę.
   	return 1;
   }
-  return num * factorialize(num - 1);
+  return num * factorialize(num - 1);     //W każdym innym przypadku wylicz silnię poprzez odwołanie wewnątrz funkcji do tej samej funkcji.  "Recursion" is a technique for iterating over an operation by having a function call itself repeatedly until it arrives at a result. 
 }
 
-console.log(factorialize(5));
+console.log(factorialize(5));             //Drukuje w konsoli efekt działania funkcji "factorialize" z argumentem "5".
