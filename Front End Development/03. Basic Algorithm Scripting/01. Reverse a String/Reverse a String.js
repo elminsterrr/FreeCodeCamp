53a17b5437bd7d572337495cead2dda045e83eee
elminsterrr <elminsterthewhite@gmail.com>
2016-05-04 16:16:34 +0200

    Add comments

--- Front End Development/03. Basic Algorithm Scripting/01. Reverse a String/Reverse a String.js
+++ Front End Development/03. Basic Algorithm Scripting/01. Reverse a String/Reverse a String.js
@@ -1,15 +1,11 @@
-var strSplit = "";
-
-function reverseString(str) {
-strSplit = str.split("");
-strReverse = strSplit.reverse();
-strJoin = strReverse.join("");
-  return strJoin;
+function reverseString(str) {              //Funkcja, która ma za zadanie odwrócić String podany na wejściu.
+var strSplit = str.split("");              //Metoda dzieli String na poszczególne znaki. The split() method is used to split a string into an array of substrings, and returns the new array. Tip: If an empty string ("") is used as the separator, the string is split between each character.
+var strReverse = strSplit.reverse();       //Metoda odwraca kolejność elementów w Array. The reverse() method reverses the order of the elements in an array.
+var strJoin = strReverse.join("");         //Ma połaczyć Array z poprzedniej komendy w String, który bedzie przedzielony niczym "", zmiast przecinka. The join() method joins the elements of an array into a string, and returns the string. The elements will be separated by a specified separator. The default separator is comma (,).
+  return strJoin;                          //Zwraca efekt działania funkcji reverseString. 
 }
 
-var zmiana ="";
+var zmiana = reverseString("hello");       //Zmienna "zmiana" odwołuje się do funkcji "reverseString" z argumentem ("hello").
 
-zmiana = reverseString("hello");
-
-console.log(typeof(zmiana));
-console.log(zmiana);
+console.log(typeof(zmiana));               //Pokazuje typ zmiennej "zmiana".
+console.log(zmiana);                       //Wywołuje zmienną "zmiana" i drukuje w konsoli efekt jej działania. Zmienna zmiana to funkcja "reverseString" z argumentem ("hello").
