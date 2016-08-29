6160f08384bb56842a33410f6a5c8efffbf3648b
elminsterrr <elminsterthewhite@gmail.com>
2016-08-29 20:50:24 +0200

    Backup & record of my learning process
    It is a backup copy and also a record of my learning process with Free
    Code Camp.

---
+++ Front End Development/06. Intermediate Algorithm Scripting/07. DNA Pairing/DNA Pairing 1.0.js
@@ -0,0 +1,32 @@
+/*
+Working example: http://repl.it/DAEW/5
+
+Instructions: The DNA strand is missing the pairing element.
+Take each character, get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays 
+are grouped into one encapsulating array.
+*/
+
+'use strict';
+
+function pairElement(str) {
+  var decodingTable= { A: 'T', T: 'A', C: 'G', G: 'C' };  //object literal of all DNA pairing elements
+  
+  var strSplitted = str.split('');
+  var decoded = strSplitted.map(decoder);
+  
+  function decoder(item) {
+    var dataReady = [item, decodingTable[item]];
+    return dataReady;
+  }
+  
+  return decoded;
+}
+pairElement("GCG");
