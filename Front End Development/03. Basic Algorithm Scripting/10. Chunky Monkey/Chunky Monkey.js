c7f6f807728f1c55d0d76129afda59ad5f3a0d76
elminsterrr <elminsterthewhite@gmail.com>
2016-10-02 03:53:14 +0200

    Better comments

--- Front End Development/03. Basic Algorithm Scripting/10. Chunky Monkey/Chunky Monkey.js
+++ Front End Development/03. Basic Algorithm Scripting/10. Chunky Monkey/Chunky Monkey.js
@@ -1,15 +1,18 @@
 /*
-Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+Write a function that splits an array (first argument)
+into groups the length of size (second argument)
+and returns them as a two-dimensional array.
 */
 
 function chunkArrayInGroups(arr, size) {
   var newArr = [];
-    
-  for (var i = 0; i <arr.length; i = i + size){  //W związku z i=3 ta iteracja zostanie wykonana tylko 2 razy.
-    newArr.push(arr.slice(i, i + size));         //Zaznacza 0,1,2 w pierwszym ruchu od 0,3 (bez 3), a w drugim 3,4,5 od 3 do 6 (bez 6).
+  //W związku z size=3 ( i = i + 3 ) ta iteracja zostanie wykonana tylko 2 razy.  
+  for (var i = 0; i < arr.length; i = i + size) {  
+    //Zaznacza 0,1,2 w pierwszym ruchu od 0,3 (bez 3),
+    //a w drugim 3,4,5 od 3 do 6 (bez 6).
+    newArr.push(arr.slice(i, i + size));
     console.log("Log: " + i);
   }
 return newArr;
 }
-//chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) 
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
