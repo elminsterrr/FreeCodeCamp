7b39974cba454d620c1e6cbfea7865915e5252dd
elminsterrr <elminsterthewhite@gmail.com>
2016-12-02 05:04:44 +0100

    Backup & record of my learning process
    It is a backup copy and also a record of my learning process with Free
    Code Camp.

--- Front End Development/07. Advanced Algorithm Scripting/08. Map the Debris/Map the Debris 1.0.js
+++ Front End Development/07. Advanced Algorithm Scripting/08. Map the Debris/Map the Debris 1.0.js
@@ -17,8 +17,10 @@ The radius of the earth is 6367.4447 kilometers,
 and the GM value of earth is 398600.4418 km3s-2.
 */
 function orbitalPeriod(arr) {
-   
-   function orbitalPeriodInside(inputData) {
+  
+  let results = [];
+
+  function orbitalPeriodInside(inputData) {
     const GM = 398600.4418;
     const earthRadius = 6367.4447;
     let averageAltitude = inputData.avgAlt;
@@ -39,7 +41,7 @@ function orbitalPeriod(arr) {
     return myNewObj;
   }
  
-  let results = arr.map(function(oneObj) {
+  results = arr.map(function(oneObj) {
     return orbitalPeriodInside(oneObj);
   });
 
