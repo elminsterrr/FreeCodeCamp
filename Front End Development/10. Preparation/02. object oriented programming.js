b499af739c173d457f26c089e8eb7d9f61e2e619
elminsterrr <elminsterthewhite@gmail.com>
2017-07-13 20:11:35 +0200

    Add object oriented programming

---
+++ Front End Development/10. Preparation/02. object oriented programming.js
@@ -0,0 +1,16 @@
+var x = new Object();
+console.log(x);
+
+var y = {
+  imie: 'Przemek',
+  wzrost: 179,
+  legs: 2,
+  howManyLegs: function() {
+    return this.legs;
+  }
+};
+console.log(y);
+console.log(y.wzrost);
+y.wzrost = 190;
+console.log(y.wzrost);
+console.log(y.howManyLegs());
