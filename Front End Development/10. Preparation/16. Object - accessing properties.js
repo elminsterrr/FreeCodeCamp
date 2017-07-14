0c4b033b48dd6d6a9917177c0c978c5efaf3a5cd
elminsterrr <elminsterthewhite@gmail.com>
2017-07-15 00:51:21 +0200

    Accessing properties

---
+++ Front End Development/10. Preparation/16. Object - accessing properties.js
@@ -0,0 +1,9 @@
+// Object - accessing properties
+
+const myObj = {
+  name: 'Przemek',
+  wzrost: 179
+};
+
+console.log(myObj.name); // using dot notation
+console.log(myObj['name']); // using bracket notation
