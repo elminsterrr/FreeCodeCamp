28b859d31aa359b23a6198c9be80956b3fd92068
elminsterrr <elminsterthewhite@gmail.com>
2017-07-14 05:07:32 +0200

    Fix whitespaces

--- Front End Development/10. Preparation/12. Do czego służy metoda filter obiektu Array.js
+++ Front End Development/10. Preparation/12. Do czego służy metoda filter obiektu Array.js
@@ -1,8 +1,8 @@
-// Tworzy	ona	nową	tablicę	wywołując	przekazaną	do	niej	funkcję
-// na	rzecz	każdego	z	elementów	tablicy	bazowej.
+// Tworzy ona nową tablicę wywołując przekazaną	do niej	funkcję
+// na rzecz każdego z elementów	tablicy	bazowej.
 
-// Jeśli	funkcja	zwraca	true,	dany	element	trafia
-// do	wynikowej	tablicy.	W	przeciwnym wypadku	jest	on	pomijany.
+// Jeśli funkcja zwraca	true, dany element trafia
+// do wynikowej	tablicy. W przeciwnym wypadku jest on pomijany.
 
 const tablica = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const newArray = tablica.filter(element => element < 5);
