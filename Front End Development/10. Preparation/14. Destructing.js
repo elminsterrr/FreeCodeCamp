487a8e8551e211e7b578764f3aac9e4b598725b4
elminsterrr <elminsterthewhite@gmail.com>
2017-07-14 05:29:20 +0200

    Destructing

---
+++ Front End Development/10. Preparation/14. Destructing.js
@@ -0,0 +1,12 @@
+const wydatki = {
+  typ: 'Wakacje',
+  kwota: 3000
+};
+
+// var typ = wydatki.typ;
+// var kwota = wydatki.kwota;
+
+const { typ, kwota} = wydatki;
+
+console.log(typ);   // Wakacje
+console.log(kwota); // 3000
