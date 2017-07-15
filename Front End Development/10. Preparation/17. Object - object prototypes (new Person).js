dd787354cf1087bd4d2b795aba0c7bbd2b1553c7
elminsterrr <elminsterthewhite@gmail.com>
2017-07-15 02:17:16 +0200

    Object prototypes

---
+++ Front End Development/10. Preparation/17. Object - object prototypes (new Person).js
@@ -0,0 +1,12 @@
+function Person(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eyecolor;
+}
+
+const myFather = new Person('John', 'Doe', 50, 'blue');
+const myMother = new Person('Sally', 'Wu', 48, 'green');
+
+console.log(myFather); // { firstName: 'John', lastName: 'Doe', age: 50, eyeColor: 'blue' }
+console.log(myMother); // { firstName: 'Sally', lastName: 'Wu', age: 48, eyeColor: 'green' }
