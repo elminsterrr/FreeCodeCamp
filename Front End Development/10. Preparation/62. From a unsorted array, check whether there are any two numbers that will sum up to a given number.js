e289326602cf7a6e3e971373651481b796a6df80
elminsterrr <elminsterthewhite@gmail.com>
2017-07-27 13:45:48 +0200

    Check whether there are any two numbers // that will sum up to a given number

---
+++ Front End Development/10. Preparation/62. From a unsorted array, check whether there are any two numbers that will sum up to a given number.js
@@ -0,0 +1,21 @@
+// From a unsorted array, check whether there are any two numbers
+// that will sum up to a given number
+
+function sumFinder (arr, sum) {
+
+  let checker = false;
+
+  arr.map(function (element, elementIndex) {
+    arr.map(function (deepElement, deepElementIndex) {
+
+      if ((element + deepElement === sum) && (arr[elementIndex] !== arr[deepElementIndex])) {  // or (arr.indexOf(element) !== arr.indexOf(element))
+        checker = true;
+      }
+
+    });
+  });
+  return checker;
+}
+
+//sumFinder([6,4,3,2,1,7], 9); // true
+sumFinder([6, 4, 3, 2, 1, 7], 2); // false
