a9bd09fe2d851d9ea1aad0a9159670547947aabb
elminsterrr <elminsterthewhite@gmail.com>
2017-07-27 17:46:32 +0200

    Update

---
+++ Front End Development/10. Preparation/66. How would you compare two objects in JavaScript.js
@@ -0,0 +1,26 @@
+// How would you compare two objects in JavaScript?
+
+function compare(aObj, bObj) {
+  let aProp = Object.getOwnPropertyNames(aObj);
+  let bProp = Object.getOwnPropertyNames(bObj);
+
+  if (aProp.length !== bProp.length) return false;
+
+  for (let prop in aObj) {
+    console.log(aObj[prop], '===', bObj[prop]);
+    if (aObj[prop] !== bObj[prop]) return false;
+  }
+  return true;
+}
+
+let one = {
+  name: 'Przemek',
+  age: 17
+};
+
+let two = {
+  name: 'Przemek',
+  age: 5
+};
+
+compare(one, two); // false
