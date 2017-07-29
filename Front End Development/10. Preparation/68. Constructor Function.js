caf57eb0466b702105284a925b50714776dd7b4d
elminsterrr <elminsterthewhite@gmail.com>
2017-07-29 18:17:02 +0200

    'Update'

---
+++ Front End Development/10. Preparation/68. Constructor Function.js
@@ -0,0 +1,21 @@
+function Bike(wheels, seats, engine) {
+  this.wheels = wheels;
+  this.seats = seats;
+  this.engine = engine;
+
+  let gear = 0;
+
+  this.setGear = function(change) {
+    gear = change;
+  };
+
+  this.getGear = function() {
+    return gear;
+  };
+}
+
+let myBike = new Bike(4, 5, 1);
+console.log(myBike);
+
+myBike.setGear(6);
+myBike.getGear(); // -> 6
