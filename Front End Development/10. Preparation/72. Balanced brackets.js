04e639c502f92b03925f14d0f7da678db433d344
elminsterrr <elminsterthewhite@gmail.com>
2017-08-02 18:06:44 +0200

    Update

---
+++ Front End Development/10. Preparation/72. Balanced brackets.js
@@ -0,0 +1,15 @@
+function balanced(str) {
+  return !str
+    .split('')
+    .reduce(function(prev, char) {
+      if (prev < 0) return prev;
+      if (char === '(') return ++prev;
+      if (char === ')') return --prev;
+      return prev;
+    }, 0);
+}
+
+// balanced('()'); // true
+// balanced(')('); // false
+//balanced('(((('); // false
+balanced('(()'); // false
